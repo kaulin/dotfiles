6d217e242b946ec905c2c77cfdfd454e8f1dc2cc
Jaakko Junttila <jajuntti@student.hive.fi>
2024-06-13 16:19:56 +0300

    Update - Thu Jun 13 04:19:56 PM EEST 2024

--- update_cfgs.sh
+++ update_cfgs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# dotfiles = location of config repo
 DIR="$HOME/dotfiles/"
+FILES=$(ls -pa $DIR | grep -Ev '(/|*\.sh)')
 PREVIOUS=$(pwd)
 
 cd $DIR
 git pull
-cp -vfut $HOME .vimrc .aliases .bashrc .zshrc
+cp -vfut $HOME $FILES
 cd $PREVIOUS
